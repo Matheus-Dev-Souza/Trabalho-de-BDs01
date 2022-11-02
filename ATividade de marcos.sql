8bb620883954edb19cb6b4d157343a0b766b4271
Matheus Gonçalves Caicó <96189442+Matheus-Dev-Souza@users.noreply.github.com>
2022-11-02 14:57:50 -0300

    Add files via upload

---
+++ ATividade de marcos.sql
@@ -0,0 +1,11 @@
+insert into produto
+(codigo, nome, valor_unitário)
+values
+('1', 'Alan', '5');
+
+insert into produto
+(codigo, nome, valor_unitário)
+values
+('4','feijão','5');
+
+select * from produto;
